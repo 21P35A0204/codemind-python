4e1038656fb349c8b7a62e9f14db7ef69dc112db
21P35A0204 <103246768+21P35A0204@users.noreply.github.com>
2022-08-11 11:44:48 +0530

    n=int(input()) a,b=0,1 print(a,b,end=" ") for i in range(2,n):     c=a+b     print(c,end=' ')     a=b     b=c

--- fibonacci.py
+++ fibonacci.py
@@ -1,8 +1,8 @@
-def fibno(n):
-    fb=[0,1]
-    for i in range(2,n):
-        fb.append(fb[i-1]+fb[i-2])
-    return fb
 n=int(input())
-print(*fibno(n))
- 
+a,b=0,1
+print(a,b,end=" ")
+for i in range(2,n):
+    c=a+b
+    print(c,end=' ')
+    a=b
+    b=c
