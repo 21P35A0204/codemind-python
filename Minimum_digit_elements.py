c5f00598a4234d71cfbc9a2bf2402c1c46b71a8a
21P35A0204 <103246768+21P35A0204@users.noreply.github.com>
2022-09-08 11:00:58 +0530

    n=int(input()) l=[int(i) for i in input().split()] p=min(l) d=[] for i in l:     if len(str(i))==len(str(p)):         d.append(i) print(len(d))

--- Minimum_digit_elements.py
+++ Minimum_digit_elements.py
@@ -1,16 +1,8 @@
 n=int(input())
 l=[int(i) for i in input().split()]
-def lent(k):
-    d=0
-    while k>0:
-        r=k%10
-        d+=1
-        k=k//10
-    return d
-g=min(l)
-c=0
+p=min(l)
+d=[]
 for i in l:
-    if lent(i)==lent(g):
-        c+=1
-print(c)
-    
+    if len(str(i))==len(str(p)):
+        d.append(i)
+print(len(d))
